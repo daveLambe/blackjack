5420e7b198d04015370467af9e0eef86e25bd5a6
Dave Lambe <dave.lambe@locationlabs.com>
2017-10-13 22:15:52 -0700

    Add some logic for dealer bust and hit, must hit till 17. Bust not working as trying to separate methods

--- blackjack.py
+++ blackjack.py
@@ -21,7 +21,6 @@ class Deck(object):
 
     def shuffle_deck(self):
         shuffle(self.deck_of_cards)
-        print self.deck_of_cards
 
     def deal_card(self):
         return self.deck_of_cards[0]
@@ -52,11 +51,11 @@ class Player(object):
         DECK.remove_dealt_card()
 
     def print_current_hand_and_value(self):
-        print 'Your current hand is:', ', '.join(self.current_hand)
+        print self.__class__.__name__, 'current hand is:', ', '.join(self.current_hand)
         if self.soft_hand_value != self.hard_hand_value:
-            print 'This hand has a Soft Value of', self.soft_hand_value, 'and a Hard Value of', self.hard_hand_value
+            print self.__class__.__name__, 'hand has a Soft Value of', self.soft_hand_value, 'and a Hard Value of', self.hard_hand_value
         else:
-            print 'This hand has a value of:', self.hard_hand_value
+            print self.__class__.__name__, 'hand has a value of:', self.hard_hand_value
 
     def calc_hand_value(self):
         self.soft_hand_value = 0
@@ -72,16 +71,21 @@ class Player(object):
                 self.soft_hand_value += 10
                 self.hard_hand_value += 10
 
-    def check_win_hand(self):
+    def check_win_or_bust(self):
         if self.soft_hand_value == 21 or self.hard_hand_value == 21:
-            print 'Player has won!'
+            print self.__class__.__name__, 'has won!'
             return True
-        elif self.soft_hand_value > 21 or self.hard_hand_value > 21:
-            print 'Bust!'
-            return True
+        # elif self.soft_hand_value > 21 or self.hard_hand_value > 21:
+        #     print self.__class__.__name__, 'has bust!'
+        #     return True
         else:
             return False
 
+    def check_bust(self):
+        if self.soft_hand_value > 21 or self.hard_hand_value > 21:
+            return True
+        else:
+            return False
 
 
 class Gambler(Player):
@@ -107,7 +111,7 @@ class Gambler(Player):
     def change_bet_amount(self, new_bet_amount):
         self.bet_amount = new_bet_amount
 
-    def player_turn(self):
+    def gambler_turn(self):
         GAMBLER.print_current_hand_and_value()
         GAMBLER.print_bankroll()
         ask_player = raw_input('Hit or Stay?')
@@ -116,7 +120,8 @@ class Gambler(Player):
             GAMBLER.calc_hand_value()
             GAMBLER.print_current_hand_and_value()
         elif ask_player == 'stay':
-            print 'You chose to stay'
+            print 'Dealers turn'
+            DEALER.dealer_turn()
 
 
 class Dealer(Player):
@@ -129,6 +134,14 @@ class Dealer(Player):
 
 # Methods
 
+    def dealer_turn(self):
+        while DEALER.soft_hand_value < 17 and DEALER.hard_hand_value < 17:
+            DEALER.ask_hit()
+            DEALER.calc_hand_value()
+            DEALER.print_current_hand_and_value()
+            DEALER.check_win_or_bust()
+        print 'At least 17 reached'
+
 
 
 # End Classes
@@ -136,38 +149,91 @@ class Dealer(Player):
 
 
 # Start Logic
-
+print
+print
 print "Let's play BlackJack!"
+print
+print
 
-print 'Deck INFO'
+
+# SUDO CODE!
+# Make a Deck
+game_over = False
 DECK = Deck()
-DECK.print_deck()
+# Shuffle deck
 DECK.shuffle_deck()
-print
-
-print 'GAMBLER INFO'
+# make a gambler
 GAMBLER = Gambler()
-GAMBLER.ask_hit()
-GAMBLER.ask_hit()
-GAMBLER.print_current_hand_and_value()
-GAMBLER.calc_hand_value()
-GAMBLER.check_win_hand()
-print
-
-print 'DEALER INFO'
+# make a dealer
 DEALER = Dealer()
+# give gambler two cards
+GAMBLER.ask_hit()
+GAMBLER.ask_hit()
+# give dealer two cards
 DEALER.ask_hit()
 DEALER.ask_hit()
+# calc gambler hand and value
+GAMBLER.calc_hand_value()
+# print gambler hand and value
 GAMBLER.print_current_hand_and_value()
-DEALER.check_win_hand()
 print
+print
+# calc dealer hand and value
+DEALER.calc_hand_value()
+# print dealers hand and value
+DEALER.print_current_hand_and_value()
+print
+print
+# check if gambler wins already
+GAMBLER.check_win_or_bust()
+# check if dealer wins already
+DEALER.check_win_or_bust()
+
+
+while not GAMBLER.check_win_or_bust() or DEALER.check_win_or_bust():
+    GAMBLER.gambler_turn()
+
+# If not, gambler can hit or stay
+# if hit:
+#     give new card
+#     see if they win or bust
+#     continue
+# if stay:
+#     give dealer cards until at least 17
+#     see if dealer bust or win
+#     if not bust or 21:
+#         check if dealer or player higher
+
+
+# print 'Deck INFO'
+# DECK = Deck()
+# DECK.print_deck()
+# DECK.shuffle_deck()
+# print
+
+# print 'GAMBLER INFO'
+# GAMBLER = Gambler()
+# GAMBLER.ask_hit()
+# GAMBLER.ask_hit()
+# GAMBLER.print_current_hand_and_value()
+# GAMBLER.calc_hand_value()
+# GAMBLER.check_win_or_bust()
+
+
+# print 'DEALER INFO'
+# DEALER = Dealer()
+# DEALER.ask_hit()
+# DEALER.ask_hit()
+# DEALER.print_current_hand_and_value()
+# DEALER.check_win_or_bust()
 
 
 
-print 'Logic printout:'
+# print 'Logic printout:'
 
-while not GAMBLER.check_win_hand():
-    GAMBLER.player_turn()
+# while not GAMBLER.check_win_or_bust():
+#     GAMBLER.player_turn()
+
 
 
 

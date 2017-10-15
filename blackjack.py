1a2e946e4f23ff97b618b2dd8426dc4fc2c4529b
Dave Lambe <dave.lambe@locationlabs.com>
2017-10-15 15:58:55 -0700

    Redid all logic. Should have commited more frequently. Most things working but game still asks gambler for hit or stay following a win/bust

--- blackjack.py
+++ blackjack.py
@@ -46,11 +46,12 @@ class Player(object):
 
 # Methods
 
-    def ask_hit(self):
+    def deal_card(self):
         self.current_hand.append(DECK.deck_of_cards[0])
         DECK.remove_dealt_card()
 
     def print_current_hand_and_value(self):
+        self.calc_hand_value()
         print self.__class__.__name__, 'current hand is:', ', '.join(self.current_hand)
         if self.soft_hand_value != self.hard_hand_value:
             print self.__class__.__name__, 'hand has a Soft Value of', self.soft_hand_value, 'and a Hard Value of', self.hard_hand_value
@@ -92,6 +93,14 @@ class Player(object):
         else:
             return False
 
+    def check_highest_hand(self):
+        if GAMBLER.hard_hand_value == DEALER.hard_hand_value:
+            print 'Tied hand!'
+        elif GAMBLER.hard_hand_value > DEALER.hard_hand_value:
+            print 'Gambler wins by Higher Valued hand!'
+        else:
+            print 'Dealer wins by Higher Valued hand!'
+
 class Gambler(Player):
     """
     Subclass - Gambler
@@ -116,19 +125,19 @@ class Gambler(Player):
         self.bet_amount = new_bet_amount
 
     def gambler_turn(self):
-        global gambler_turn_over
-        GAMBLER.calc_hand_value()
-        GAMBLER.print_current_hand_and_value()
         GAMBLER.print_bankroll()
         ask_player = raw_input('Hit or Stay?')
         if ask_player == 'hit':
-            GAMBLER.ask_hit()
-            GAMBLER.calc_hand_value()
-            GAMBLER.print_current_hand_and_value()
+            self.deal_card()
+            self.print_current_hand_and_value()
+            return True
         elif ask_player == 'stay':
-            gambler_turn_over = True
             print 'Dealers turn - from gambler_turn'
-            DEALER.dealer_turn()
+            self.gambler_turn_over()
+            return False
+
+    def gambler_turn_over(self):
+        return True
 
 
 class Dealer(Player):
@@ -143,88 +152,186 @@ class Dealer(Player):
 
     def dealer_turn(self):
         global game_over
-        if DEALER.soft_hand_value < 17 and DEALER.hard_hand_value < 17:
-            DEALER.ask_hit()
-            DEALER.calc_hand_value()
-            DEALER.print_current_hand_and_value()
-            DEALER.check_win()
-            DEALER.check_bust()
-        elif DEALER.soft_hand_value == 21 or DEALER.hard_hand_value == 21:
-                DEALER.print_current_hand_and_value()
-                game_over = True
-                print 'Dealer wins at 21! - from dealer turn'
+        if self.soft_hand_value < 17 and self.hard_hand_value < 17:
+            self.deal_card()
+            return True
+        # elif self.soft_hand_value == 21 or self.hard_hand_value == 21:
+        #     self.print_current_hand_and_value()
+        #     game_over = True
+        #     print 'Dealer wins at 21! - from dealer turn'
+        #     return False
         else:
-            DEALER.print_current_hand_and_value()
-            game_over = True
-            print 'dealer reached at least 17'
+            # self.print_current_hand_and_value()
+            # game_over = True
+            # print 'dealer reached at least 17'
+            return False
 
 # End Classes
 
 
 # Start Logic
+
+
+# Last working code
+# # SUDO CODE!
+# # Make a Deck
+# gambler_turn_over = False
+# game_over = False
+# DECK = Deck()
+# # Shuffle deck
+# DECK.shuffle_deck()
+# # make a gambler
+# GAMBLER = Gambler()
+# # make a dealer
+# DEALER = Dealer()
+# # give gambler two cards
+# GAMBLER.ask_hit()
+# GAMBLER.ask_hit()
+# # give dealer two cards
+# DEALER.ask_hit()
+# DEALER.ask_hit()
+# # calc gambler hand and value
+# #GAMBLER.calc_hand_value()
+# # print gambler hand and value
+# #GAMBLER.print_current_hand_and_value()
+# print
+# print
+# # calc dealer hand and value
+# DEALER.calc_hand_value()
+# # print dealers hand and value
+# DEALER.print_current_hand_and_value()
+# print
+# print
+# # check if gambler wins already
+# GAMBLER.check_win()
+# DEALER.check_win()
+
+# Sudo Main logic - Start Game
 print
 print
 print "Let's play BlackJack!"
 print
 print
-
-
-# SUDO CODE!
-# Make a Deck
-gambler_turn_over = False
 game_over = False
 DECK = Deck()
-# Shuffle deck
 DECK.shuffle_deck()
-# make a gambler
 GAMBLER = Gambler()
-# make a dealer
+GAMBLER.deal_card()
+GAMBLER.deal_card()
 DEALER = Dealer()
-# give gambler two cards
-GAMBLER.ask_hit()
-GAMBLER.ask_hit()
-# give dealer two cards
-DEALER.ask_hit()
-DEALER.ask_hit()
-# calc gambler hand and value
-#GAMBLER.calc_hand_value()
-# print gambler hand and value
-#GAMBLER.print_current_hand_and_value()
+DEALER.deal_card()
+DEALER.deal_card()
+
+GAMBLER.print_current_hand_and_value()
 print
-print
-# calc dealer hand and value
-DEALER.calc_hand_value()
-# print dealers hand and value
 DEALER.print_current_hand_and_value()
-print
-print
-# check if gambler wins already
-GAMBLER.check_win()
-DEALER.check_win()
+
+# Sudo Main logic - Check for WIN on OPEN
+if GAMBLER.check_win():
+    print 'Gambler gets BlackJack on open!'
+    game_over = True
+elif GAMBLER.check_bust():
+    print 'Gambler busts on open!'
+    game_over = True
+
+if DEALER.check_win():
+    print 'Dealer gets BlackJack on open!'
+    game_over = True
+elif DEALER.check_bust():
+    print 'Dealer busts on open!'
+    game_over = True
 
 
-while not game_over:
-    if gambler_turn_over:
-        DEALER.dealer_turn()
-        DEALER.calc_hand_value()
-        DEALER.check_win()
-        DEALER.check_bust()
-    else:
-        GAMBLER.gambler_turn()
-        GAMBLER.calc_hand_value()
-        GAMBLER.check_win()
-        GAMBLER.check_bust()
 
-if GAMBLER.check_win() or DEALER.check_bust():
-        print 'Gambler Wins with a hand value of', GAMBLER.hard_hand_value
-elif DEALER.check_win() or GAMBLER.check_bust():
-    print 'Dealer Wins with a hand value of', DEALER.hard_hand_value
-elif GAMBLER.hard_hand_value > DEALER.hard_hand_value:
-    print 'Gambler Wins with a hand value of', GAMBLER.hard_hand_value
-elif DEALER.hard_hand_value > GAMBLER.hard_hand_value:
-    print 'Dealer Wins with a hand value of', DEALER.hard_hand_value
+# Sudo Main logic - Nobody wins on open
+if not game_over:
+    while GAMBLER.gambler_turn():
+        if GAMBLER.check_win():
+            print 'Gambler wins NOT ON OPEN'
+            game_over = True
+        elif GAMBLER.check_bust():
+            print 'Gambler busts NOT ON OPEN'
+            game_over = True
+        else:
+            GAMBLER.gambler_turn()
+            GAMBLER.check_bust()
+            GAMBLER.check_win()
+            GAMBLER.print_current_hand_and_value()
+
+    while DEALER.dealer_turn():
+        if DEALER.check_win():
+            DEALER.print_current_hand_and_value()
+            print 'Dealer wins NOT ON OPEN'
+            game_over = True
+        elif DEALER.check_bust():
+            DEALER.print_current_hand_and_value()
+            print 'Dealer busts NOT ON OPEN'
+            game_over = True
+        else:
+            DEALER.print_current_hand_and_value()
+
+    GAMBLER.check_highest_hand()
+    game_over = True
+
 else:
-    print 'Tie!'
+    print 'Game is over following all logic. game_over = True'
+
+# Sudo Main Logic
+# Create Deck ---
+# Shuffle Deck ---
+# Create Gambler ---
+# Create Dealer ---
+# Give Gambler two cards ---
+# Give Dealer two cards ---
+# Check if either wins on open ---
+
+# If win:
+# present win ---
+# ask player if they want to play again
+#
+# If not win:
+# Ask Gambler hit or stay ---
+# If hit, deal card ---
+# Check if they win - If so, tell win and ask if play again
+# Check if they bust - If so, tell lose and ask if play again
+#
+# Repeat until they say stay
+# After stay:
+# When they say stay:
+# If dealer below 17, give dealer card.
+# Check if dealer win
+# Check if dealer bust
+# Repeat until dealer gets to at least 17
+#
+# If nobody win or bust by here, simply check who has higher card value or if tie.
+# Present winner or tie and ask if player again
+
+
+
+
+# Last working code
+# while not game_over:
+#     if gambler_turn_over:
+#         DEALER.dealer_turn()
+#         DEALER.calc_hand_value()
+#         DEALER.check_win()
+#         DEALER.check_bust()
+#     else:
+#         GAMBLER.gambler_turn()
+#         GAMBLER.calc_hand_value()
+#         GAMBLER.check_win()
+#         GAMBLER.check_bust()
+#
+# if GAMBLER.check_win() or DEALER.check_bust():
+#         print 'Gambler Wins with a hand value of', GAMBLER.hard_hand_value
+# elif DEALER.check_win() or GAMBLER.check_bust():
+#     print 'Dealer Wins with a hand value of', DEALER.hard_hand_value
+# elif GAMBLER.hard_hand_value > DEALER.hard_hand_value:
+#     print 'Gambler Wins with a hand value of', GAMBLER.hard_hand_value
+# elif DEALER.hard_hand_value > GAMBLER.hard_hand_value:
+#     print 'Dealer Wins with a hand value of', DEALER.hard_hand_value
+# else:
+#     print 'Tie!'
 
 
 

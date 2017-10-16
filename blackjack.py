65740518ced8c51890be19ce53ad7e6f0a6c5a1c
Dave Lambe <dave.lambe@locationlabs.com>
2017-10-16 00:19:30 -0700

    Fixed most pylint errors

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,8 @@
+"""
+BlackJack Game!
+Has a Gambler versus an automated dealer.
+Dealer hits on 17!
+"""
 from random import shuffle
 # BlackJack Game
 
@@ -11,21 +16,41 @@ class Deck(object):
 # Methods
 
     def __init__(self):
+        """
+        Uses list comprehension to make a deck of cards.
+        """
         values = range(2, 10) + ('Jack King Queen Ace').split()
         suits = 'Diamonds Clubs Spades Hearts'.split()
         self.deck_of_cards = ['%s of %s' % (v, s) for v in values for s in suits]
 
     def print_deck(self):
+        """
+        Prints deck
+        :return:
+        """
         print self.deck_of_cards
         print len(self.deck_of_cards)
 
     def shuffle_deck(self):
+        """
+        Uses Shuffle from random to shuffle deck
+        :return:
+        """
         shuffle(self.deck_of_cards)
 
     def deal_card(self):
+        """
+        Deals first card in Deck
+        (Deck is shuffled once at start so taking first should be okay)
+        :return:
+        """
         return self.deck_of_cards[0]
 
     def remove_dealt_card(self):
+        """
+        Removes the first card in deck. Only used after dealing, therefore removing the dealt card from deck.
+        :return:
+        """
         del self.deck_of_cards[0]
 
 
@@ -35,10 +60,15 @@ class Player(object):
     Has attributes and methods used by both
     """
 
-
-# Base Class - Player (Gambler & Dealer)
-
     def __init__(self, current_hand=[], soft_hand_value=0, hard_hand_value=0, hit=False, stand=False):
+        """
+        init for Base Player Class - (Gambler & Dealer)
+        :param current_hand: Current cards in hand - List
+        :param soft_hand_value: Value of hand using Ace as 1 = int
+        :param hard_hand_value: Value of hand using Ace as 11 = int
+        :param hit: if you want to hit - Bool
+        :param stand: If you want to stand - Bool
+        """
         self.current_hand = current_hand
         self.soft_hand_value = soft_hand_value
         self.hard_hand_value = hard_hand_value
@@ -48,10 +78,20 @@ class Player(object):
 # Methods
 
     def deal_card(self):
+        """
+        Deals card and appends to hand
+        Removes dealt card from Deck
+        :return:
+        """
         self.current_hand.append(DECK.deck_of_cards[0])
         DECK.remove_dealt_card()
 
     def print_current_hand_and_value(self):
+        """
+        Calls method to calc hand value, then prints out all cards in current hand and the value of the hand
+        If you have an Ace, it will show soft & hard value
+        :return:
+        """
         self.calc_hand_value()
         print self.__class__.__name__, 'hand contains:', ', '.join(self.current_hand)
         if self.soft_hand_value != self.hard_hand_value:
@@ -60,6 +100,10 @@ class Player(object):
             print self.__class__.__name__, 'hand has a value of:', self.hard_hand_value
 
     def calc_hand_value(self):
+        """
+        Calculates value of current hand
+        :return:
+        """
         self.soft_hand_value = 0
         self.hard_hand_value = 0
         for i in range(len(self.current_hand)):
@@ -74,28 +118,41 @@ class Player(object):
                 self.hard_hand_value += 10
 
     def check_win(self):
+        """
+        Checks if you have won by hitting 21 with either soft or hard hand value
+        :return:
+        """
         # Sudo hard vs soft
         # if players hard hand value is greater than 21
         #     players hard hand value = players soft hand value
-        global game_over
+        global GAME_OVER
         if self.soft_hand_value == 21 or self.hard_hand_value == 21:
             print self.__class__.__name__, 'has won! - from check win'
-            game_over = True
+            GAME_OVER = True
             return True
         else:
             return False
 
     def check_bust(self):
-        global game_over
+        """
+        Checks if soft value is over 21, if so.. bust.
+        :return:
+        """
+        global GAME_OVER
         if self.soft_hand_value > 21:
             print self.__class__.__name__, 'is bust! - from check bust'
-            game_over = True
+            GAME_OVER = True
             return True
         else:
             return False
 
     def check_use_hard_or_soft(self):
-        global winner
+        """
+        Determines if you should/can use soft or hard hand value.
+        If your hard and soft value are different (And hard value will not make you bust), then you want to use hard value.
+        In this case, your hard value becomes your hard and soft value and you can try use it to win.
+        :return:
+        """
         print
         if self.soft_hand_value != self.hard_hand_value:
             if self.hard_hand_value < 21:
@@ -103,19 +160,25 @@ class Player(object):
             else:
                 print 'Hard value too high to use'
 
-    def check_winning_hand(self, a, b):
-        global winner
+    def check_winning_hand(self, gam, deal):
+        """
+        Checks tie or who won
+        :param gam:
+        :param deal:
+        :return:
+        """
+        global WINNER
         print
-        print 'Final score.. \nDealer: %s \nGambler: %s' % (b.soft_hand_value, a.soft_hand_value)
-        if a.soft_hand_value == b.soft_hand_value:
+        print 'Final score.. \nDealer: %s \nGambler: %s' % (deal.soft_hand_value, gam.soft_hand_value)
+        if gam.soft_hand_value == deal.soft_hand_value:
             print 'Tied hand!'
-            winner = 'Nobody!'
-        elif a.soft_hand_value > b.soft_hand_value:
+            WINNER = 'Nobody!'
+        elif gam.soft_hand_value > deal.soft_hand_value:
             print 'Gambler wins by Higher Valued hand!'
-            winner = 'Gambler'
+            WINNER = 'Gambler'
         else:
             print 'Dealer wins by Higher Valued hand!'
-            winner = 'Dealer'
+            WINNER = 'Dealer'
 
 
 class Gambler(Player):
@@ -131,18 +194,36 @@ class Gambler(Player):
 # Methods
 
     def print_bankroll(self):
+        """
+        Prints current amount in Gamblers bank
+        :return:
+        """
         print 'Your bank contains', self.bankroll
         print
         print
 
     def add_bankroll(self, new_bankroll):
+        """
+        Adds to Gamblers bankroll
+        :param new_bankroll:
+        :return:
+        """
         self.bankroll += new_bankroll
 
     def change_bet_amount(self, new_bet_amount):
+        """
+        Default betting amount is 5, this will allow you to raise. (Not used yet)
+        :param new_bet_amount:
+        :return:
+        """
         self.bet_amount = new_bet_amount
 
     def gambler_turn(self):
-        global gambler_turn_over
+        """
+        Goes through flow of gamblers turn
+        :return:
+        """
+        global GAMBLER_TURN_OVER
         # GAMBLER.print_bankroll()
         print
         ask_player = raw_input('Hit or Stay?')
@@ -153,7 +234,7 @@ class Gambler(Player):
         elif ask_player == 'stay':
             print 'Here is the final Gambler hand...'
             self.print_current_hand_and_value()
-            gambler_turn_over = True
+            GAMBLER_TURN_OVER = True
             return False
 
 
@@ -162,24 +243,36 @@ class Dealer(Player):
     Subclass - Dealer
     """
     def __init__(self, current_hand=[], soft_hand_value=0, hard_hand_value=0, hit=False, stand=False):
+        """
+        Creates Dealer subclass
+        :param current_hand:
+        :param soft_hand_value:
+        :param hard_hand_value:
+        :param hit:
+        :param stand:
+        """
         Player.__init__(self, current_hand, soft_hand_value, hard_hand_value, hit, stand)
 
 
 # Methods
 
     def dealer_turn(self):
-        global game_over
-        global dealer_turn_over
+        """
+        Goes through flow of dealer turn
+        :return:
+        """
+        global GAME_OVER
+        global DEALER_TURN_OVER
         print '\nDealers turn..'
         if self.hard_hand_value < 21:
             if self.hard_hand_value < 17:
-                'Dealer needs to draw a card'
+                print 'Dealer needs to draw a card'
                 self.deal_card()
                 DEALER.print_current_hand_and_value()
                 return True
             else:
                 print 'Dealer does not need to draw a card'
-                dealer_turn_over = True
+                DEALER_TURN_OVER = True
                 return False
         elif self.soft_hand_value < 17:
             print 'Dealer needs to draw a card'
@@ -188,7 +281,7 @@ class Dealer(Player):
             return True
         else:
             print 'Dealer does not need to draw a card'
-            dealer_turn_over = True
+            DEALER_TURN_OVER = True
             return False
 
 
@@ -201,10 +294,10 @@ print
 print "Let's play BlackJack!"
 print
 print
-game_over = False
-gambler_turn_over = False
-dealer_turn_over = False
-winner = 'blank'
+GAME_OVER = False
+GAMBLER_TURN_OVER = False
+DEALER_TURN_OVER = False
+WINNER = 'blank'
 DECK = Deck()
 DECK.shuffle_deck()
 GAMBLER = Gambler()
@@ -222,84 +315,65 @@ DEALER.print_current_hand_and_value()
 
 if GAMBLER.check_win():
     print 'Gambler gets BlackJack on open!'
-    game_over = True
-    winner = 'Gambler'
+    GAME_OVER = True
+    WINNER = 'Gambler'
 elif GAMBLER.check_bust():
     print 'Gambler busts on open!'
-    game_over = True
-    winner = 'Dealer'
+    GAME_OVER = True
+    WINNER = 'Dealer'
 
 if DEALER.check_win():
     print 'Dealer gets BlackJack on open!'
-    game_over = True
-    winner = 'Dealer'
+    GAME_OVER = True
+    WINNER = 'Dealer'
 elif DEALER.check_bust():
     print 'Dealer busts on open!'
-    game_over = True
-    winner = 'Gambler'
+    GAME_OVER = True
+    WINNER = 'Gambler'
 
 # Main logic - If nobody wins on Open
 
-while not game_over:
+while not GAME_OVER:
 
-    while not gambler_turn_over:
+    while not GAMBLER_TURN_OVER:
         if GAMBLER.check_win():
-            gambler_turn_over = True
-            game_over = True
-            winner = 'GAMBLER'
+            GAMBLER_TURN_OVER = True
+            GAME_OVER = True
+            WINNER = 'GAMBLER'
             print
             print 'Gambler hits 21!!'
         elif GAMBLER.check_bust():
-            winner = 'DEALER'
+            WINNER = 'DEALER'
             print
             print 'Gambler hits bust!!!'
-            gambler_turn_over = True
-            game_over = True
+            GAMBLER_TURN_OVER = True
+            GAME_OVER = True
         else:
             GAMBLER.gambler_turn()
 
-    if not game_over:
-        while not dealer_turn_over:
+    if not GAME_OVER:
+        while not DEALER_TURN_OVER:
             if DEALER.check_win():
                 print
-                winner = 'DEALER'
+                WINNER = 'DEALER'
                 print 'Dealer hits 21!!'
-                dealer_turn_over = True
-                game_over = True
+                DEALER_TURN_OVER = True
+                GAME_OVER = True
             elif DEALER.check_bust():
                 print
-                winner = 'Gambler'
+                WINNER = 'Gambler'
                 print 'Dealer hits bust!!'
-                dealer_turn_over = True
-                game_over = True
+                DEALER_TURN_OVER = True
+                GAME_OVER = True
             else:
                 DEALER.dealer_turn()
 
-    if not game_over:
+    if not GAME_OVER:
         GAMBLER.check_use_hard_or_soft()
         DEALER.check_use_hard_or_soft()
         GAMBLER.check_winning_hand(GAMBLER, DEALER)
-        game_over = True
+        GAME_OVER = True
 
 print
 print
-print 'Winner of the hand is %s' % winner
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print 'Winner of the hand is %s' % WINNER
